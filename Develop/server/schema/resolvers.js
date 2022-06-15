0bc9863515b727f5c1a46d27eb410673d3ef14c0
Damaris Palacios <dslpz728@gmail.com>
2022-06-15 12:33:32 -0500

    adds to resolvers js and typedef.js

---
+++ Develop/server/schema/resolvers.js
@@ -0,0 +1,34 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+
+
+
+const resolvers = {
+    Query: {
+      // logic here remains the same
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+      
+        return user;
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        return user;
+  
+      }
+    }
+  };
+  module.exports = resolvers;
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +28,10 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
 });
+
+startApolloServer(typeDefs, resolvers);
+
