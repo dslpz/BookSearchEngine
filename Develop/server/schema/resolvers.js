a14ac8b74ee41c6a2f6c7dc13b3426edef9c7d1c
Damaris Palacios <dslpz728@gmail.com>
2022-06-15 20:56:48 -0500

    installs apollo client

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 
 
@@ -8,27 +9,28 @@ const resolvers = {
       // logic here remains the same
     },
     Mutation: {
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
-      
-        return user;
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-      
-        if (!user) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-      
-        const correctPw = await user.isCorrectPassword(password);
-      
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-      
-        return user;
-  
-      }
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const token = signToken(user);
+            return { token, user };
     }
-  };
+    }
+}
   module.exports = resolvers;
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -11,7 +12,8 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
