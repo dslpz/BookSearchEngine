90b7814f5320a12220e70d2bb07a560107080f92
Damaris Palacios <dslpz728@gmail.com>
2022-06-15 21:49:12 -0500

    changes to server ja

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -18,7 +18,7 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
